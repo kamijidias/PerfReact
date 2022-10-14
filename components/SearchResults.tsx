42f4d696ae6b9fe64bed2499732dfae66290a5ba
Andrew Kamiji Dias <andrew_kamiji_dias@hotmail.com>
2022-10-14 14:21:48 -0300

    React virtualized

--- components/SearchResults.tsx
+++ components/SearchResults.tsx
@@ -1,4 +1,5 @@
-import { useMemo } from "react";
+import { List, ListRowRenderer } from "react-virtualized";
+
 import { ProductItem } from "./ProductItem";
 
 interface SearchResultProps {
@@ -12,23 +13,34 @@ interface SearchResultProps {
   onAddToWishList: (id: number) => void;
 }
 
-export function SearchResults({ totalPrice, results, onAddToWishList }: SearchResultProps) {
+export function SearchResults({
+  totalPrice,
+  results,
+  onAddToWishList,
+}: SearchResultProps) {
+  const rowRenderer: ListRowRenderer = ({ index, key, style }) => {
+    return (
+      <div key={key} style={style}>
+        <ProductItem
+          product={results[index]}
+          onAddToWishList={onAddToWishList}
+        />
+      </div>
+    );
+  };
 
   return (
     <div>
       <h2>{totalPrice}</h2>
 
-      {results.map(product => {
-        return (
-          <>
-            <ProductItem 
-            key={product.id}
-            product={product} 
-            onAddToWishList = {onAddToWishList}
-            />
-          </>
-        );
-      })}
+      <List
+        height={300}
+        rowHeight={30}
+        width={900}
+        overscanRowCount={5}
+        rowCount={results.length}
+        rowRenderer={rowRenderer}
+      />
     </div>
   );
 }
