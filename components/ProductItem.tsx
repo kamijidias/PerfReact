9063e50e1ff15a5efa687164e1b3bad5b1ba5817
Andrew Kamiji Dias <andrew_kamiji_dias@hotmail.com>
2022-10-14 14:56:10 -0300

    Bundle Analyzer

--- components/ProductItem.tsx
+++ components/ProductItem.tsx
@@ -1,6 +1,7 @@
 import { memo, useState } from "react";
 import { AddProductToWishListProps } from "./AddProductToWishList";
 import dynamic from "next/dynamic";
+import lodash from 'lodash'
 
 //import { AddProductToWishList } from "./AddProductToWishList";
 
@@ -43,6 +44,6 @@ function ProductItemComponent({ product, onAddToWishList }: ProductItemProps) {
 export const ProductItem = memo(
   ProductItemComponent,
   (prevProps, nextProps) => {
-    return Object.is(prevProps.product, nextProps.product);
+    return lodash.isEqual(prevProps.product, nextProps.product);
   }
 );
